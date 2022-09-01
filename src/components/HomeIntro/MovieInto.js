e6865429cca9e5bdd7705ab8cf727aacfac1d7fc
alissonpatrickk <alisson.souz.pa@gmail.com>
2022-09-01 00:29:01 -0300

    infos css

--- src/components/HomeIntro/MovieInto.js
+++ src/components/HomeIntro/MovieInto.js
@@ -3,6 +3,14 @@ import './styles.css';
 
 export default ({ item }) => {
     console.log(item)
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name )
+    }
+
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -10,7 +18,20 @@ export default ({ item }) => {
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
             <div className="featured-vertical">
-                <div className="featured-horizontal"></div>
+                <div className="featured-horizontal">
+                    <div className="featured-name">{item.original_name}</div>
+                    <div className="featured-info">
+                        <div className="featured-points">{item.vote_average}</div>
+                        <div className="featured-year">{firstDate.getFullYear()}</div>
+                        <div className="featured-seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className="featured-description">{item.overview}</div>
+                    <div className="featured-buttons">
+                        <a className="button-watch" href={`/watch/${item.id}`}>► Assistir</a>
+                        <a className="button-list" href="{`/watch/${item.id}`}">+ Minha Lista </a>
+                    </div>
+                    <div className="featured-genres">Genres: <strong>{genres.join(', ')}</strong></div>
+                </div>
             </div>
         </section>
     )
